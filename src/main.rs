d651c308e9f16af66626e1023584cd9412419b12
François Mockers <mockersf@gmail.com>
2017-05-28 00:25:01 +0200

    using hyper for http

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,102 @@
 extern crate futures;
-extern crate futures_cpupool;
-extern crate num_cpus;
-extern crate tokio_proto;
-extern crate tokio_service;
-extern crate url;
-extern crate bytes;
-extern crate tokio_io;
+
 extern crate tokio_core;
-extern crate httparse;
+extern crate tokio_proto;
+
+extern crate hyper;
+
+#[macro_use]
+extern crate lazy_static;
+
 extern crate libloading as lib;
 
-extern crate mini_http;
 extern crate module_interface;
-extern crate module_logger_request;
 
-use std::sync::mpsc;
-use std::thread;
-use std::net::ToSocketAddrs;
-use std::{io, str};
-use futures::future::{self, Either};
-use futures::{BoxFuture, Future};
-use tokio_proto::TcpServer;
-use tokio_service::Service;
-use tokio_core::net::TcpStream;
+use std::cell::Cell;
+use std::sync::{Arc, Mutex};
 
-use mini_http::request::Request;
-use mini_http::response::Response;
-use mini_http::Http;
+use futures::Future;
+
+use tokio_core::reactor::Remote;
+
+use hyper::StatusCode;
+use hyper::header::Host;
+use hyper::server::{Http, Service, Request, Response};
+use hyper::Client;
+
+
+use std::str;
 
 mod lib_loader;
 
+lazy_static! {
+    static ref REMOTE: Arc<Mutex<Cell<Option<Remote>>>> = Arc::new(Mutex::new(Cell::new(None)));
+}
+
+thread_local! {
+    static CLIENT: Client<hyper::client::HttpConnector> =
+            Client::new(&REMOTE.lock().unwrap().get_mut().clone().unwrap().handle().unwrap());
+}
+
+
+#[derive(Clone, Copy)]
+struct Config {
+    target: &'static str,
+    target_port: u16,
+}
+
 struct Gateway {
-    remote_handle: tokio_core::reactor::Remote,
     input_modules: Vec<Box<module_interface::InputModule>>,
+    config: Config,
+}
+
+impl Gateway {
+    fn forwarded_request(&self, req: Request) -> Request {
+        let url = match req.uri().query() {
+                Some(qp) => {
+                    format!("http://{}:{}{}?{}",
+                            self.config.target,
+                            self.config.target_port,
+                            req.path(),
+                            qp)
+                }
+                None => {
+                    format!("http://{}:{}{}",
+                            self.config.target,
+                            self.config.target_port,
+                            req.path())
+                }
+            }
+            .parse::<hyper::Uri>()
+            .unwrap();
+        let mut forwarded_request = req;
+        forwarded_request
+            .headers_mut()
+            .set::<Host>(Host::new(self.config.target, self.config.target_port));
+        forwarded_request.set_uri(url);
+        forwarded_request
+    }
 }
 
 impl Service for Gateway {
     type Request = Request;
     type Response = Response;
-    type Error = std::io::Error;
-    type Future = Either<future::Ok<Response, io::Error>, BoxFuture<Response, io::Error>>;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item = Self::Response, Error = hyper::Error>>;
 
     fn call(&self, req: Request) -> Self::Future {
-        match req.headers()
-                  .into_iter()
-                  .find(|header| header.0 == "Host")
-                  .map(|header| header.1) {
-            Some("127.0.0.1:8080") => Either::A(future::ok(self.internal(req))),
-            Some("localhost:8080") => Either::B(self.forward(req)),
-            _ => {
-                let mut resp = Response::new();
-                resp.status_code(404, "Not Found");
-                Either::A(future::ok(resp))
-            }
+        match req.headers().get::<hyper::header::Host>().cloned() {
+            Some(ref host) if host.hostname() == "127.0.0.1" => self.internal(req),
+            Some(ref host) if host.hostname() == "localhost" => self.forward(req),
+            _ => futures::future::ok(Response::new().with_status(StatusCode::NotFound)).boxed(),
         }
     }
 }
 
 impl Gateway {
-    fn internal(&self, req: Request) -> Response {
+    fn internal(&self, req: Request) -> Box<Future<Item = Response, Error = hyper::Error>> {
         println!("internal request, do some config: {:?}", req);
-        let mut resp = Response::new();
-        resp.header("Content-Type", "text/plain")
-            .body("configure me");
-        resp
+        futures::future::ok(Response::new().with_status(StatusCode::Accepted)).boxed()
     }
 
     fn do_input_modules(&self, req: &Request) {
@@ -72,75 +105,58 @@ impl Gateway {
         }
     }
 
-    fn forward(&self, req: Request) -> BoxFuture<Response, io::Error> {
-        let (tx, rx) = futures::oneshot();
-
+    fn forward(&self, req: Request) -> Box<Future<Item = Response, Error = hyper::Error>> {
         self.do_input_modules(&req);
 
-        self.remote_handle
-            .spawn(move |handle| {
-                let addr = "127.0.0.1:1026".to_socket_addrs().unwrap().next().unwrap();
-                TcpStream::connect(&addr, handle)
-                    .and_then(move |socket| tokio_io::io::write_all(socket, req.data))
-                    .and_then(|(socket, _request)| tokio_io::io::read_to_end(socket, Vec::new()))
-                    .and_then(|(_socket, data)| {
-                                  let a = tx.send(data);
-                                  a.unwrap();
-                                  Ok(())
+        let forwarded_request = self.forwarded_request(req);
+        CLIENT.with(|client| {
+            Box::new(client
+                         .request(forwarded_request)
+                         .map(|res| {
+                                  let res: Response = res;
+                                  if res.status() != StatusCode::Ok {
+                                      println!("{:?}", res);
+                                  }
+                                  res
                               })
-                    .map_err(|_| ())
-            });
+                         .or_else(|err| {
+                println!("{:?}", err);
+                futures::future::ok(Response::new().with_status(StatusCode::ServiceUnavailable))
+            }))
+        })
 
-        let data = match rx.wait() {
-            Ok(data) => data,
-            Err(_) => {
-                let v: Vec<u8> = vec![];
-                v
-            }
-        };
-        future::ok(Response::raw(data)).boxed()
     }
 }
-use std::clone::Clone;
+
 fn main() {
-    let addr = "0.0.0.0:8080".parse().unwrap();
+    let addr = "127.0.0.1:3000".parse().unwrap();
 
-    let (tx, rx) = mpsc::channel();
+    let config = Config {
+        target: "localhost",
+        target_port: 8080,
+    };
 
-    thread::spawn(move || {
-        let mut core = match tokio_core::reactor::Core::new() {
-            Ok(core) => core,
-            Err(err) => {
-                tx.send(Err(err)).expect("Channel was closed early");
-                return;
-            }
-        };
+    let module_path = "module-logger-request-dynamic/target/release/libmodule_logger_request.dylib";
+    let logger_module = lib_loader::LoadedInputModule::load(module_path);
+    let module_path = "module-auth/target/release/libmodule_auth.dylib";
+    let auth_module = lib_loader::LoadedInputModule::load(module_path);
 
-        tx.send(Ok(core.remote()))
-            .expect("Channel was closed early");
+    let server = Http::new()
+        .bind(&addr, move || {
+            Ok(Gateway {
+                   input_modules: {
+                       vec![Box::new(logger_module.clone()), Box::new(auth_module.clone())]
+                   },
+                   config,
+               })
+        })
+        .unwrap();
+    println!("Listening on http://{} with 1 thread.",
+             server.local_addr().unwrap());
 
-        loop {
-            core.turn(None);
-        }
-    });
-
-    let remote = rx.recv().unwrap();
-    let remote: tokio_core::reactor::Remote = remote.unwrap();
-    let mut srv = TcpServer::new(Http, addr);
-    srv.threads(num_cpus::get());
-
-    let logger_module = lib_loader::LoadedInputModule::load("aa");
-    let logger_module2 = lib_loader::LoadedInputModule::load("bb");
-
-    srv.serve(move || {
-        Ok(Gateway {
-               remote_handle: remote.clone(),
-               input_modules: {
-                   vec![Box::new(logger_module.clone()),
-                        Box::new(logger_module2.clone())
-                        //Box::new(module_logger_request::LoggerRequest {})
-                    ]
-               },
-           })
-    })
+    REMOTE
+        .lock()
+        .unwrap()
+        .set(Some(server.handle().remote().clone()));
+    server.run().unwrap();
 }
